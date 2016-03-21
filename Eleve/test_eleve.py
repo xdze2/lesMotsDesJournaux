5bb17ee7613a11f4a43f2761c2f5d7453e251d00
xdze2 <iopzae@gmail.com>
2016-03-21 23:09:04 +0100

    test EleVe

--- Eleve/test_eleve.py
+++ Eleve/test_eleve.py
@@ -5,30 +5,39 @@ filename = 'data4eleve.json'
 
 data = json.loads(open(filename).read())
 
-print( 'nbr phrases: %i ' % len( data ) )
+print( 'nombre de phrases: %i ' % len( data ) )
 
 
 from eleve import MemoryStorage
 storage = MemoryStorage()
 
 # Then the training itself:
+n = 0
 for sentence in data:
-      storage.add_sentence( sentence)
+      n += len( sentence )
+      storage.add_sentence( sentence )
 
-
+print( 'nombre de mots: %i ' % n )
 
 
 from eleve import Segmenter
 s = Segmenter(storage)
 # segment up to 4-grams, if we used the same storage as before.
 
-nuplets = []
-for phrase in data[:100]:
+nuplets_count = {}
+for phrase in data:
       segmentedPhrase = s.segment( phrase )
-      for mots in segmentedPhrase:
-            if len( mots )>1:
-                  mots = ' '.join( mots )
-                  nuplets.append( mots )
+      for nuplet in segmentedPhrase:
+            if len( nuplet )>1:
+                  nuplet = ' '.join( nuplet )
+                  if nuplet in nuplets_count:
+                        nuplets_count[ nuplet ] += 1
+                  else:
+                        nuplets_count[ nuplet ] = 1
 
-print( ' / '.join(nuplets) )
+sorted_nuplets = sorted( nuplets_count.items(), key=lambda x:x[1], reverse=True )
+
+output = [ x[0]+' (%i)'%x[1] for x in sorted_nuplets ]
+
+print( '; '.join( output) )
 

acc7fd128c33870bee427de551701a66c9ef11e5
xdze2 <iopzae@gmail.com>
2016-04-06 20:27:23 +0200

    ajout fichier manquant

---
+++ web/js_files/horizontal.js
@@ -0,0 +1,46 @@
+jQuery(function($){
+	'use strict';
+
+	// -------------------------------------------------------------
+	//   Basic Navigation
+	// -------------------------------------------------------------
+	(function () {
+		var $frame  = $('#myTimeLine');
+		var $slidee = $frame.children('ul').eq(0);
+		var $wrap   = $frame.parent();
+
+		// Call Sly on frame
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'basic',
+			smart: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 0,
+			scrollBar: $wrap.find('.scrollbar'),
+			scrollBy: 1,
+                  scrollTrap: 1,
+                  scrollSource:  $frame,
+			pagesBar: $wrap.find('.pages'),
+			activatePageOn: 'click',
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+			// Buttons
+			forward: $wrap.find('.forward'),
+			backward: $wrap.find('.backward'),
+			prev: $wrap.find('.prev'),
+			next: $wrap.find('.next'),
+			prevPage: $wrap.find('.prevPage'),
+			nextPage: $wrap.find('.nextPage')
+		});
+
+	}());
+
+
+});
---
+++ web/js_files/myScript.js
@@ -0,0 +1,140 @@
+var data_filename = 'data.json'
+$.getJSON(data_filename, function (data) {
+		// get elements
+		var $frame  = $('#myTimeLine');
+		var $slidee = $frame.children('ul').eq(0);
+		var $wrap   = $frame.parent();
+
+		// window size
+		var windowHeight = $( window ).height();
+		var windowWidth = $( window ).width() - 5*2 ;// 5:margin
+
+		$wrap.css({ height : windowHeight - 10  }); // safety margin
+
+		var maxHeight = windowHeight - 150;
+
+		var targetedDayWidth = 360;
+		var nFramesVisibles =Math.ceil( windowWidth/targetedDayWidth );
+		var actualDayWidth = Math.round( windowWidth / nFramesVisibles );
+		var nFramesTot = data.length;
+
+    // data
+		$.each( data, function(i, d){
+			var day = d['day']
+			var blocks = d['mots']
+
+			$slidee.append( $('<li />')
+				.attr('id', day )
+				.css( {width:actualDayWidth, height:maxHeight} )
+				.html( '<h3>'+ d['labelDay'] +'</h3> <p></p>' )
+			);
+
+			fillAday( day, blocks, maxHeight  );
+		});
+		console.log( $slidee.children('li').length );
+		/* - Param and call Sly  (scrolling lib) - */
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'basic',
+			smart: 1,
+			activateOn: null, //'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: nFramesTot - nFramesVisibles,
+			scrollBar: $wrap.find('.scrollbar'),
+			scrollBy: 1,
+      scrollTrap: 1,
+      scrollSource:  $('body'),//$frame,
+			pagesBar: $wrap.find('.pages'),
+			activatePageOn: 'click',
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1
+		});
+});
+
+
+function fillAday( id, blocks, maxHeight  ){
+	// selectionne le jour
+	var $mardi = $( '#' + id + ' p' );
+	$mardi.css({ position:'relative', height:maxHeight});
+
+	// insert les mots dans le DOM pour avoir leurs taille
+	$.each( blocks, function(i, d){
+		var $mot = $('<span />', {
+			id : d['id'],
+			css : { fontSize : d['size']+"em"	,
+							padding: '0px 5px 0px 0px', // t r b l
+							'white-space': 'nowrap',
+							position : 'absolute',
+							'line-height': '90%'
+						 }
+		});
+		var label = arrangeLabel( d['label'] )
+		$mot.html( label ); // insert html: pas cool ??
+		$mot.appendTo( $mardi )
+
+		d['w'] = $mot.outerWidth();
+		d['h'] = $mot.outerHeight();
+
+		var maxWidth = $mardi.width();
+
+		if (d['w']>maxWidth ){
+		/* ReScale la taille de police, si mot trop long */
+			var newSize = d['size']/d['w']*maxWidth*0.97;
+			$mot.css( {'fontSize': newSize+'em' } ); //, "font-weight":"bold"
+			d['w'] = $mot.outerWidth();
+			d['h'] = $mot.outerHeight();
+			//console.log( 'not fit '+d['label']+' '+newSize );
+		}
+
+	});
+
+	// go for Packer
+	var wFrame = $mardi.width();
+	var hFrame = 10*$mardi.height();
+
+	blocks.sort( function(a, b){
+		var aire = b.w*b.h - a.w*a.h;
+		return aire;
+	});
+	packer = new Packer( wFrame, hFrame );
+	packer.fit(blocks);
+
+	// move the blocks
+	$.each( blocks, function(i, d){
+		var $mot = $( '#'+d['id'] );
+
+		if (d.fit){
+			$mot.css( {  top:d.fit.y, left:d.fit.x } );
+		} else { console.log( ' no fit.packer '+d['label'] ); }
+
+	});
+
+
+};
+
+
+function arrangeLabel( label ){
+// ajoute un retour a la ligne quand mots de la même longueur
+	var mots = label.split(" ");
+
+	if( mots.length === 2 && Math.abs(mots[0].length - mots[1].length)<3 ){
+		// console.log( label );
+		return mots[0]+'<br />'+mots[1];
+	} else { 	return label;  }
+}
+
+function dayLabel( day ){
+// formate le 'jour' en français
+	var chiffres = day.split("-");
+
+	if( mots.length === 2 && Math.abs(mots[0].length - mots[1].length)<3 ){
+		// console.log( label );
+		return mots[0]+'<br />'+mots[1];
+	} else { 	return label;  }
+}
